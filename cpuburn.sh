23fd82ab11fbbf0e4f3de125afda9ab08b2fb1d4
Davenport <41927748+davenport651@users.noreply.github.com>
2019-09-11 09:15:31 -0400

    Update cpuburn.sh
    updated for xmrig 3.x.x

--- cpuburn.sh
+++ cpuburn.sh
@@ -4,14 +4,12 @@
 username="15ufDkou9LJrdWTizkVTkAeXTfFC7HT13t.cputest$RANDOM"
 url="cryptonightr.usa-new.nicehash.com:3375"
 
-cd /root/cpuburn/
-
 cat /proc/cpuinfo | grep "model name" | head -1
 echo $username
 #echo $url
 echo Burn in progress...
-echo "Ctrl + C to abort"
+echo "[H] to report H/s; [Ctrl] + [C] to abort..."
 
 #Execute CPU burn-in test
-#./xmrig --url=$url --user=$username --pass="x" --variant=-1 --asm=auto --nicehash --donate-level=40 --print-time=500 &> /dev/null
-./xmrig --url=$url --user=$username --pass="x" --variant=-1 --asm=auto --nicehash --donate-level=40 --print-time=500 | grep speed
+#./xmrig --algo="cn/r" --url=$url --user=$username --pass="x" --asm=auto --nicehash --donate-level=40 --print-time=500
+./xmrig --algo="cn/r" --url=$url --user=$username --pass="x" --asm=auto --nicehash --donate-level=40 --print-time=500 | grep speed

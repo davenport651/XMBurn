d126ebb0cb8dc79f1ffeb19356c9ad6e9334b841
Davenport <41927748+davenport651@users.noreply.github.com>
2019-09-19 10:13:55 -0400

    Update xmburn.sh
    Added private pool example

--- xmburn.sh
+++ xmburn.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 #variables
-username="15ufDkou9LJrdWTizkVTkAeXTfFC7HT13t.cputest$RANDOM"
-url="cryptonightr.usa-new.nicehash.com:3375"
+#Nicehash version:
+#username="15ufDkou9LJrdWTizkVTkAeXTfFC7HT13t.cputest$RANDOM"
+#url="cryptonightr.usa-new.nicehash.com:3375"
+#Private monero pool:
+username="45ynYARcmKcLZrB1kEfjsbCcGByjVqsCnhzAgLpN1xjnTzxRRp8F7tq3bXbnrW929mdRcyBSAHNTzjHap4Wgbc8FTWRaut9.cputest$RANDOM"
+url="xmr-usa.dwarfpool.com:8050"
 
 cat /proc/cpuinfo | grep "model name" | head -1
 echo $username
@@ -12,4 +16,5 @@ echo "[H] to report H/s; [Ctrl] + [C] to abort..."
 
 #Execute CPU burn-in test
 #./xmrig --algo="cn/r" --url=$url --user=$username --pass="x" --asm=auto --nicehash --donate-level=40 --print-time=500
-./xmrig --algo="cn/r" --url=$url --user=$username --pass="x" --asm=auto --nicehash --donate-level=40 --print-time=500 | grep speed
+#./xmrig --algo="cn/r" --url=$url --user=$username --pass="x" --asm=auto --nicehash --donate-level=40 --print-time=500 | grep speed
+./xmrig --algo=cn/r --url=$url --user=$username --pass="x" --asm=auto --keepalive --donate-level=40 --print-time=500 | grep speed

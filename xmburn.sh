71b292d4c189f0138d8ecb3bf0147125361b6906
Davenport <41927748+davenport651@users.noreply.github.com>
2019-10-25 09:55:30 -0400

    Update xmburn.sh
    Added colors and reworked output

--- xmburn.sh
+++ xmburn.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+white=$'\e[0m'
+
 #variables
 #Nicehash version:
-#username="15ufDkou9LJrdWTizkVTkAeXTfFC7HT13t.cputest$RANDOM"
-#url="cryptonightr.usa-new.nicehash.com:3375"
+username="15ufDkou9LJrdWTizkVTkAeXTfFC7HT13t.cputest$RANDOM"
+url="cryptonightr.usa-new.nicehash.com:3375"
 #Private monero pool:
-username="45ynYARcmKcLZrB1kEfjsbCcGByjVqsCnhzAgLpN1xjnTzxRRp8F7tq3bXbnrW929mdRcyBSAHNTzjHap4Wgbc8FTWRaut9.cputest$RANDOM"
-url="xmr-usa.dwarfpool.com:8050"
+#username="45ynYARcmKcLZrB1kEfjsbCcGByjVqsCnhzAgLpN1xjnTzxRRp8F7tq3bXbnrW929mdRcyBSAHNTzjHap4Wgbc8FTWRaut9.cputest$RANDOM"
+#url="xmr-usa.dwarfpool.com:8050"
 
-cat /proc/cpuinfo | grep "model name" | head -1
-echo $username
-#echo $url
-echo Burn in progress...
-echo "[H] to report H/s; [Ctrl] + [C] to abort..."
+echo $cyn $username $white
+#echo $cyn $url $white
+echo $mag Burn in progress... $white
+
 
 #Execute CPU burn-in test
 #./xmrig --algo="cn/r" --url=$url --user=$username --pass="x" --asm=auto --nicehash --donate-level=40 --print-time=500
-#./xmrig --algo="cn/r" --url=$url --user=$username --pass="x" --asm=auto --nicehash --donate-level=40 --print-time=500 | grep speed
-./xmrig --algo=cn/r --url=$url --user=$username --pass="x" --asm=auto --keepalive --donate-level=40 --print-time=500 | grep speed
+./xmrig --algo="cn/r" --url=$url --user=$username --pass="x" --asm=auto --nicehash --donate-level=40 --print-time=500 | egrep 'accepted|speed|(CPU.*AES)|esume|reject'
+#./xmrig --algo=cn/r --url=$url --user=$username --pass="x" --asm=auto --keepalive --donate-level=40 --print-time=500 | egrep 'accepted|speed|(CPU.*AES)|esume|reject'

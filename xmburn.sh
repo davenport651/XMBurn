15e841bc384e339a289388b6e7b22f0d23b73182
Davenport <41927748+davenport651@users.noreply.github.com>
2019-11-04 22:40:02 -0500

    Update xmburn.sh
    Replaced private pool example.

--- xmburn.sh
+++ xmburn.sh
@@ -9,8 +9,8 @@ white=$'\e[0m'
 username="15ufDkou9LJrdWTizkVTkAeXTfFC7HT13t.cputest$RANDOM"
 url="cryptonightr.usa-new.nicehash.com:3375"
 #Private monero pool:
-#username="45ynYARcmKcLZrB1kEfjsbCcGByjVqsCnhzAgLpN1xjnTzxRRp8F7tq3bXbnrW929mdRcyBSAHNTzjHap4Wgbc8FTWRaut9.cputest$RANDOM"
-#url="xmr-usa.dwarfpool.com:8050"
+#username="45ynYARcmKcLZrB1kEfjsbCcGByjVqsCnhzAgLpN1xjnTzxRRp8F7tq3bXbnrW929mdRcyBSAHNTzjHap4Wgbc8FTWRaut9"
+#url="pool.supportxmr.com:5555"
 
 echo $cyn $username $white
 #echo $cyn $url $white
@@ -20,4 +20,4 @@ echo $mag Burn in progress... $white
 #Execute CPU burn-in test
 #./xmrig --algo="cn/r" --url=$url --user=$username --pass="x" --asm=auto --nicehash --donate-level=40 --print-time=500
 ./xmrig --algo="cn/r" --url=$url --user=$username --pass="x" --asm=auto --nicehash --donate-level=40 --print-time=500 | egrep 'accepted|speed|(CPU.*AES)|esume|reject'
-#./xmrig --algo=cn/r --url=$url --user=$username --pass="x" --asm=auto --keepalive --donate-level=40 --print-time=500 | egrep 'accepted|speed|(CPU.*AES)|esume|reject'
+#./xmrig --algo=cn/r --url=$url --user=$username --pass="cputest$RANDOM" --asm=auto --keepalive --donate-level=40 --print-time=500 | egrep 'accepted|speed|(CPU.*AES)|esume|reject'

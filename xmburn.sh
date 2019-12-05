464504ce803ec01f8e007c546a6d8f5c6dd73620
Davenport <41927748+davenport651@users.noreply.github.com>
2019-12-05 00:44:19 -0500

    Update xmburn.sh
    Update post-RandomX fork; added support for huge pages

--- xmburn.sh
+++ xmburn.sh
@@ -4,10 +4,14 @@ mag=$'\e[1;35m'
 cyn=$'\e[1;36m'
 white=$'\e[0m'
 
+#These two lines activate huge pages when run as root
+core=`nproc --all`
+sysctl -w vm.nr_hugepages=$core > /dev/null 2>&1
+
 #variables
 #Nicehash version:
 username="15ufDkou9LJrdWTizkVTkAeXTfFC7HT13t.cputest$RANDOM"
-url="cryptonightr.usa-new.nicehash.com:3375"
+url="randomxmonero.usa.nicehash.com:3380"
 #Private monero pool:
 #username="45ynYARcmKcLZrB1kEfjsbCcGByjVqsCnhzAgLpN1xjnTzxRRp8F7tq3bXbnrW929mdRcyBSAHNTzjHap4Wgbc8FTWRaut9"
 #url="pool.supportxmr.com:5555"
@@ -18,6 +22,6 @@ echo $mag Burn in progress... $white
 
 
 #Execute CPU burn-in test
-#./xmrig --algo="cn/r" --url=$url --user=$username --pass="x" --asm=auto --nicehash --donate-level=40 --print-time=500
-./xmrig --algo="cn/r" --url=$url --user=$username --pass="x" --asm=auto --nicehash --donate-level=40 --print-time=500 | egrep 'accepted|speed|(CPU.*AES)|esume|reject'
-#./xmrig --algo=cn/r --url=$url --user=$username --pass="cputest$RANDOM" --asm=auto --keepalive --donate-level=40 --print-time=500 | egrep 'accepted|speed|(CPU.*AES)|esume|reject'
+#./xmrig --coin="monero" --url=$url --user=$username --pass="x" --nicehash --donate-level=40 --print-time=500
+./xmrig --coin="monero" --url=$url --user=$username --pass="x" --nicehash --donate-level=40 --print-time=500 | egrep 'accepted|speed|(CPU.*AES)|esume|reject'
+#./xmrig --coin="monero" --url=$url --user=$username --pass="cputest$RANDOM" --keepalive --donate-level=40 --print-time=500 | egrep 'accepted|speed|(CPU.*AES)|esume|reject'

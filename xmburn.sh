8cedb5e0fd4384a2d7c9197fa18b2b4095f106c0
Davenport <41927748+davenport651@users.noreply.github.com>
2019-12-16 12:33:12 -0500

    Update xmburn.sh
    added 1gb-pages option for xmrig > 5.2; added name variable to generate worker name.

--- xmburn.sh
+++ xmburn.sh
@@ -3,6 +3,7 @@
 mag=$'\e[1;35m'
 cyn=$'\e[1;36m'
 white=$'\e[0m'
+name="TEST$RANDOM"
 
 #These two lines activate huge pages when run as root
 core=`nproc --all`
@@ -10,18 +11,18 @@ sysctl -w vm.nr_hugepages=$core > /dev/null 2>&1
 
 #variables
 #Nicehash version:
-username="15ufDkou9LJrdWTizkVTkAeXTfFC7HT13t.cputest$RANDOM"
+username="15ufDkou9LJrdWTizkVTkAeXTfFC7HT13t.$name"
 url="randomxmonero.usa.nicehash.com:3380"
 #Private monero pool:
 #username="45ynYARcmKcLZrB1kEfjsbCcGByjVqsCnhzAgLpN1xjnTzxRRp8F7tq3bXbnrW929mdRcyBSAHNTzjHap4Wgbc8FTWRaut9"
 #url="pool.supportxmr.com:5555"
 
-echo $cyn $username $white
+echo $cyn This system: $name $white
 #echo $cyn $url $white
 echo $mag Burn in progress... $white
 
 
 #Execute CPU burn-in test
-#./xmrig --coin="monero" --url=$url --user=$username --pass="x" --nicehash --donate-level=40 --print-time=500
-./xmrig --coin="monero" --url=$url --user=$username --pass="x" --nicehash --donate-level=40 --print-time=500 | egrep 'accepted|speed|(CPU.*AES)|esume|reject'
-#./xmrig --coin="monero" --url=$url --user=$username --pass="cputest$RANDOM" --keepalive --donate-level=40 --print-time=500 | egrep 'accepted|speed|(CPU.*AES)|esume|reject'
+#./xmrig --coin="monero" --url=$url --user=$username --pass="x" --nicehash --randomx-1gb-pages --donate-level=40 --print-time=500
+./xmrig --coin="monero" --url=$url --user=$username --pass="x" --nicehash --randomx-1gb-pages --donate-level=40 --print-time=500 | egrep 'accepted|speed|(CPU.*AES)|esume|reject'
+#./xmrig --coin="monero" --url=$url --user=$username --pass="$name" --keepalive --randomx-1gb-pages --donate-level=40 --print-time=500 | egrep 'accepted|speed|(CPU.*AES)|esume|reject'

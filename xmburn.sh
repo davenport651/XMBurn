eaad4dc32db4be3400510d1001d0b8fc20b20582
Davenport <41927748+davenport651@users.noreply.github.com>
2020-01-07 10:05:42 -0500

    Update xmburn.sh
    replaced "variables" section with case statement to check user arguments

--- xmburn.sh
+++ xmburn.sh
@@ -3,19 +3,30 @@
 mag=$'\e[1;35m'
 cyn=$'\e[1;36m'
 white=$'\e[0m'
-name="TEST$RANDOM"
+name="CPUTEST$RANDOM"
 
 #These two lines activate huge pages when run as root
 core=`nproc --all`
 sysctl -w vm.nr_hugepages=$core > /dev/null 2>&1
 
-#variables
-#Nicehash version:
-username="15ufDkou9LJrdWTizkVTkAeXTfFC7HT13t.$name"
-url="randomxmonero.usa.nicehash.com:3380"
-#Private monero pool:
-#username="45ynYARcmKcLZrB1kEfjsbCcGByjVqsCnhzAgLpN1xjnTzxRRp8F7tq3bXbnrW929mdRcyBSAHNTzjHap4Wgbc8FTWRaut9"
-#url="pool.supportxmr.com:5555"
+case "$1" in
+       nh)
+        username="15ufDkou9LJrdWTizkVTkAeXTfFC7HT13t.$name"
+        url="randomxmonero.usa.nicehash.com:3380"
+        args="--coin="monero" --url=$url --user=$username --pass="x" --nicehash --randomx-1gb-pages --donate-level=40 --print-time=500 | egrep 'accepted|speed|(CPU.*AES)|esume|reject'"
+        ;;
+        
+       pvt)
+        username="45ynYARcmKcLZrB1kEfjsbCcGByjVqsCnhzAgLpN1xjnTzxRRp8F7tq3bXbnrW929mdRcyBSAHNTzjHap4Wgbc8FTWRaut9"
+        url="pool.supportxmr.com:5555"
+        args="--coin="monero" --url=$url --user=$username --pass="$name" --keepalive --randomx-1gb-pages --donate-level=40 --print-time=500 | egrep 'accepted|speed|(CPU.*AES)|esume|reject'"
+        ;;
+        
+       *)
+          echo $"Usage: $0 {nh|prv}"
+          exit 1
+esac 
+        
 
 echo $cyn This system: $name $white
 #echo $cyn $url $white
@@ -23,6 +34,5 @@ echo $mag Burn in progress... $white
 
 
 #Execute CPU burn-in test
-#./xmrig --coin="monero" --url=$url --user=$username --pass="x" --nicehash --randomx-1gb-pages --donate-level=40 --print-time=500
-./xmrig --coin="monero" --url=$url --user=$username --pass="x" --nicehash --randomx-1gb-pages --donate-level=40 --print-time=500 | egrep 'accepted|speed|(CPU.*AES)|esume|reject'
-#./xmrig --coin="monero" --url=$url --user=$username --pass="$name" --keepalive --randomx-1gb-pages --donate-level=40 --print-time=500 | egrep 'accepted|speed|(CPU.*AES)|esume|reject'
+./xmrig $args
+
